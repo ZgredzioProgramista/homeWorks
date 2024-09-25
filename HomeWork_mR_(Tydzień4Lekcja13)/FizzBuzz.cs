b942ff6864fc88dc50b895e81fe48acb552e4297
ZgredzioProgramista <marek-rusko@wp.pl>
2024-09-25 21:10:25 +0200

    Praca Tydzień 4Lekcja13

---
+++ HomeWork_mR_(Tydzień4Lekcja13)/FizzBuzz.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_mR__Tydzień4Lekcja13_
+{
+
+    internal class FizzBuzz
+    {
+       
+        public int FizzBuzzGame() 
+        {
+            
+            Console.WriteLine("Witaj w \"FizzBuzz\" podaj liczbę: ");
+            while (true)
+            {
+               
+                try
+                {   
+                    Console.WriteLine("Wcisnij 0000 - aby zakończyć");
+                    if (!int.TryParse(Console.ReadLine(), out int input))
+                    {
+                        Console.WriteLine("Nieprawidlowe dane");                    
+                       
+                    }
+                    else if (input % 3 == 0 && input % 5 == 0)
+                    {
+                        Console.WriteLine("FizzBuzz");
+                        
+                    }
+                    else if (input % 3 == 0)
+                    {
+                        Console.WriteLine("Fizz");
+                        
+                    }
+                    else if (input % 5 == 0)
+                    {
+                        Console.WriteLine("Buzz");
+                        
+                    }
+                    else if (input % 3 != 0 || input % 5 != 0)
+                    {
+                        Console.WriteLine(input);
+                        
+                    }
+                    if (input == 0000)
+                    {
+                        Environment.Exit(0);
+                        
+                    }
+     
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+               
+
+            }
+           
+         
+
+        }
+       
+
+    }
+   
+}
---
+++ HomeWork_mR_(Tydzień4Lekcja13)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_mR__Tydzień4Lekcja13_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var FizzBuzz = new FizzBuzz();
+            FizzBuzz.FizzBuzzGame();
+     
+        }
+
+
+    }
+}
